75d67db8e09eaee9ee1509672e8b2b2bafcbb9ef
voltkigisa <raden.233040043@mail.unpas.ac.id>
2025-05-07 11:51:33 +0700

    pertemuan 10

---
+++ pertemuan10/Node.java
@@ -0,0 +1,26 @@
+package pertemuan10;
+
+public class Node {
+    private Matakuliah Data;
+    private Node Next;
+    
+    public Node(Matakuliah Data) {
+        this.Data = Data;
+    }
+
+    public Matakuliah getData() {
+        return Data;
+    }
+
+    public void setData(Matakuliah data) {
+        Data = data;
+    }
+
+    public Node getNext() {
+        return Next;
+    }
+
+    public void setNext(Node next) {
+        Next = next;
+    }
+}
